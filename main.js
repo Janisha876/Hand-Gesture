7c925e584c5e737db2976bb78d51b6d3d1ec4b89
Janisha876 <94450884+Janisha876@users.noreply.github.com>
2021-12-28 16:57:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,3 +16,36 @@ classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/1
 function modelLoaded(){
     console.log("model loaded");
 }
+function check(){
+    img=document.getElementById("CaptureImage");
+    classifier.classify(img,gotResult); 
+}
+function speak(){
+    var synth=window.speechSynthesis;
+    speakdata1="the prediction is"+prediction;
+    speakdata2="with an accuracy of"+accuracy;
+    var utterThis=new SpeechSynthesisUtterance(speakdata1+speakdata2);
+    synth.speak(utterThis);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML=results[0].label;
+        document.getElementById("accuracy").innerHTML=(results[0].confidence*100).toFixed(2);
+        prediction=results[0].label;
+        accuracy=results[0].confidence;
+        speak();
+        if(results[0].label=='Amazing'){
+            document.getElementById("update_emoji").innerHTML="&#128076";
+        }
+        if(results[0].label=='Best'){
+            document.getElementById("update_emoji").innerHTML="&#128077";
+        }
+        if(results[0].label=='Victory'){
+            document.getElementById("update_emoji").innerHTML="&#128406";
+        }
+    }
+}
